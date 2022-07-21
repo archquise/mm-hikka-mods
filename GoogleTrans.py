b4ecc586c642ee2f35f88955e08ac4b881574dd1
Daniel <62538561+GD-alt@users.noreply.github.com>
2022-07-22 01:34:36 +0300

    Add files via upload

--- GoogleTrans.py
+++ GoogleTrans.py
@@ -1,3 +1,23 @@
+# ---------------------------------------------------------------------------------
+#  ,_     _          
+#  |\_,-~/          
+#  / _  _ |    ,--.  🌐 This module was loaded through https://t.me/hikkamods_bot
+# (  @  @ )   / ,-'  🔓 Not licensed.
+#  \  _T_/-._( (     
+#  /         `. \    ⚠️ Owner of this bot doesn't take responsibility for any
+# |         _  \ |   errors caused by this module or this module being non-working
+#  \ \ ,  /      |   and doesn't take ownership of any copyrighted material.
+#   || |-_\__   /    
+#  ((_/`(____,-'     
+# ---------------------------------------------------------------------------------
+# Name: GoogleTrans
+# Description: Advanced Google Translate module!
+# Author: GD-alt
+# Commands:
+#  .autotranslate | .deflang   | .silentmode | .subsmode
+# .markmode   | .atlist        | .translate
+# ---------------------------------------------------------------------------------
+
 # meta developer: @minimaxno
 # meta pic: https://img.icons8.com/color/344/translate-text.png
 # requires: deep-translator
@@ -87,7 +107,7 @@ class GoogleTranslateMod(loader.Module):
         """Use language code with this command to switch basic translation language."""
         lang = utils.get_args_raw(message)
         if lang not in available_languages.values:
-            await utils.answer(message, self.strings("no_lang"))
+            await utils.answer(message, self.strings("nolang"))
         else:
             self.set("deflang", lang)
             await utils.answer(message, self.strings("setted"))
@@ -130,7 +150,14 @@ class GoogleTranslateMod(loader.Module):
         else:
             await utils.answer(message, self.strings("changed"))
 
-
+    async def deflangcmd(self, message: Message):
+        """Use language code with this command to switch basic translation language."""
+        lang = utils.get_args_raw(message)
+        if lang not in available_languages.values():
+            await utils.answer(message, self.strings("nolang"))
+        else:
+            self.set("deflang", lang)
+            await utils.answer(message, self.strings("setted"))
 
     async def silentmodecmd(self, message):
         """Use this command to switch between silent/unsilent mode."""
@@ -163,7 +190,7 @@ class GoogleTranslateMod(loader.Module):
         """Sends a list of chats, in which autotranslate is turned on."""
         laco = self._db.get(translations.__name__, "lang", "en")
         autotranslate = self.get("tr_cha")
-        alist = self.strings("alheader") + "\n\n"
+        alist = self.strings("alheader") + "\n"
         avlad = deep_translator.GoogleTranslator().get_supported_languages(as_dict=True)
         for i in autotranslate.keys():
             st_la, fi_la = autotranslate[i].split(";")
@@ -197,8 +224,7 @@ class GoogleTranslateMod(loader.Module):
 
             alist += (
                 f'<a href="tg://openmessage?{type_}_id={i.replace("-100", "")}">id{i.replace("-100", "")}</a>:'
-                f" {st_la} » {fi_la}"
-                + "\n"
+                f" {st_la} » {fi_la}" + "\n"
             )
 
         await utils.answer(message, alist)
@@ -282,4 +308,7 @@ class GoogleTranslateMod(loader.Module):
                 message.raw_text + "\n\n" + self.strings("tr-ed") + "\n\n" + translated
             )
 
-        await utils.answer(message, translated)
+        try:
+            await utils.answer(message, translated)
+        except:
+        	return
